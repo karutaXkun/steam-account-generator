31b6bb4a71716945516f885e7880d2e79ffe2030
Srinivasareddy L <sreenu.1009daf@gmail.com>
2020-03-05 12:45:54 +0530

    muneiah commit

---
+++ SteamAccCreator/Models/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SteamAccCreator.Models
+{
+    [Serializable]
+    public class Configuration
+    {
+        public MailConfig Mail { get; set; } = new MailConfig();
+        public CredentialConfig Login { get; set; } = new CredentialConfig();
+        public CredentialConfig Password { get; set; } = new CredentialConfig();
+        public GamesConfig Games { get; set; } = new GamesConfig();
+        public ProfileConfig Profile { get; set; } = new ProfileConfig();
+        public CaptchaSolvingConfig Captcha { get; set; } = new CaptchaSolvingConfig();
+        public OutputConfig Output { get; set; } = new OutputConfig();
+        public ProxyConfig Proxy { get; set; } = new ProxyConfig();
+
+        public void FixNulls()
+        {
+            Mail = Mail ?? new MailConfig();
+            Login = Login ?? new CredentialConfig();
+            Password = Password ?? new CredentialConfig();
+            Games = Games ?? new GamesConfig();
+            Profile = Profile ?? new ProfileConfig();
+            Captcha = Captcha ?? new CaptchaSolvingConfig();
+            Output = Output ?? new OutputConfig();
+            Proxy = Proxy ?? new ProxyConfig();
+        }
+
+        public Configuration Clone()
+            => this.DeepClone();
+    }
+}
